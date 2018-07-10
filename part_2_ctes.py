410ed670f5d53849fbde87f15ee6049254d381e7
Paulina Zheng <paulinaczheng@gmail.com>
2018-07-10 17:37:51 -0400

    Done.

--- part_2_ctes.py
+++ part_2_ctes.py
@@ -1,11 +1,14 @@
 def cte_deletes_duplicates():
-    return """Write
-            your
-            query
-            here"""
+    return """WITH min_id AS
+    (SELECT sale_id, MIN(id) as minimum_id FROM sales GROUP BY sale_id)
+    DELETE FROM sales
+    WHERE id NOT IN (SELECT minimum_id FROM min_id);"""
 
 def correct_above_avg_sales():
-    return """Write
-            your
-            query
-            here"""
+    return """WITH average_sales
+    AS (SELECT AVG(amount) FROM sales)
+    SELECT locations.city, sales.date_of_sale, sales.amount
+    FROM locations JOIN sales ON
+    locations.id = sales.location_id
+    WHERE sales.amount > (SELECT * FROM average_sales);
+    """
