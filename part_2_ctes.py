0560859e6b6fb4fce2a61507ae92fd90b6efc0f7
Christopher Pease <chris.m.pease@gmail.com>
2018-07-11 09:05:23 -0400

    Done.

--- part_2_ctes.py
+++ part_2_ctes.py
@@ -1,11 +1,15 @@
 def cte_deletes_duplicates():
-    return """Write
-            your
-            query
-            here"""
+    return """ WITH duplicates AS (
+    SELECT MIN(id) AS minimum_id
+    FROM sales GROUP BY sale_id)
+        DELETE FROM sales
+        WHERE id NOT IN (SELECT minimum_id FROM duplicates);
+    """
 
 def correct_above_avg_sales():
-    return """Write
-            your
-            query
-            here"""
+    return """ WITH average_sales AS (
+    SELECT AVG(amount) AS benchmark FROM sales)
+        SELECT locations.city, sales.date_of_sale, sales.amount
+    FROM sales JOIN locations ON locations.id = sales.location_id
+    WHERE amount > (SELECT benchmark FROM average_sales);
+    """
